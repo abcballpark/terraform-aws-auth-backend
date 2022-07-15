c6b3d68cb633dcaf11d8cc801d8f29a4142bc2a7
Kevin Coffey <kevdougful@gmail.com>
2022-07-15 18:09:27 -0500

    Refresh Token

--- main.tf
+++ main.tf
@@ -15,5 +15,5 @@ resource "aws_cognito_user_pool_client" "enrollment-api" {
   name                = "enrollment-api"
   user_pool_id        = aws_cognito_user_pool.main.id
   generate_secret     = true
-  explicit_auth_flows = ["ALLOW_USER_SRP_AUTH"]
+  explicit_auth_flows = ["ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
 }
