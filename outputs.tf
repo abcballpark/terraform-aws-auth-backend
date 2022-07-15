326e93ceb3c49e82a7f64c1e64d8ca05aa482f0d
Kevin Coffey <kevdougful@gmail.com>
2022-07-15 17:11:59 -0500

    Output User Pool ARN

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "pool_id" {
-  value = aws_cognito_user_pool.main.id
+  value       = aws_cognito_user_pool.main.id
   description = "Identifier for Cognito user pool"
+}
+
+output "pool_arn" {
+  value       = aws_cognito_user_pool.main.arn
+  description = "ARN for Cognito user pool"
 }
